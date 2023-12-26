ce6d49c8ad1c11e0b57dcd196f3dab1a51cd4723
Yingkai Lee <yil179@ucsd.edu>
2023-12-25 22:29:50 -0800

    Add button to change grid size

--- script.js
+++ script.js
@@ -4,26 +4,44 @@ let size = 16;
 
 bodyElement.appendChild(containerElement);
 
-containerElement.style.border = "thin solid black";
+containerElement.style.border = "thick solid black";
 containerElement.style.display = "flex";
-containerElement.style.width = "500px";
-containerElement.style.height = "500px";
+containerElement.style.width = "650px";
+containerElement.style.height = "650px";
 containerElement.style.flexDirection = "column";
 
-for (let i = 0; i < 16; i ++){
-	let rowElement = document.createElement('div');
-	for (let j = 0; j < 16; j ++){
-		let cellElement = document.createElement('div');
-		cellElement.style.border = "thin solid red";
-		cellElement.style.flexGrow = "1";
-		cellElement.addEventListener("mouseenter", function(){
-			cellElement.style.backgroundColor = "orange";
-		});
-		rowElement.appendChild(cellElement);
-		rowElement.style.flexGrow = "1";
-		rowElement.style.display = "flex";
-	}
-	containerElement.appendChild(rowElement);
-}
+let cellStorage = [];
 
+function makeGrid(newSize){
+	while(containerElement.firstChild){
+		containerElement.removeChild(containerElement.firstChild);
+	}
+	for (let i = 0; i < newSize; i ++){
+		let rowElement = document.createElement('div');
+		for (let j = 0; j < newSize; j ++){
+			let cellElement = document.createElement('div');
+			cellElement.style.border = "thin solid red";
+			cellElement.style.flexGrow = "1";
+			cellElement.addEventListener("mouseenter", function(){
+				cellElement.style.backgroundColor = "orange";
+			});
+			rowElement.appendChild(cellElement);
+			rowElement.style.flexGrow = "1";
+			rowElement.style.display = "flex";
+	
+			cellStorage.push(cellElement);
+		}
+		containerElement.appendChild(rowElement);
+	}
+}
+makeGrid(size);
+
+let buttonElement = document.createElement('button');
+buttonElement.textContent = "Change size";
+bodyElement.appendChild(buttonElement);
+
+buttonElement.addEventListener("click", function(){
+	size = prompt("Enter new size");
+	makeGrid(size);
+});
 
